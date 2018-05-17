537a41c6e882b4db6316b30c9be66e1269f335f7
gonzalorqn <gonzalo.rqn@gmail.com>
2018-05-17 10:24:50 -0300

    nomenclatura de punteros con enteros

--- nomenclatura/main.c
+++ nomenclatura/main.c
@@ -3,6 +3,26 @@
 
 int main()
 {
-    printf("Hello world!\n");
+    int *punteroNumero;
+    int numero;
+    int numeroDos;
+    numero=66;
+    //&numeroDos=&numero;
+    numeroDos = &numero; //le asigno la posicion de memoria
+    printf("a- %d\n",numeroDos);
+    punteroNumero = &numero;
+    numeroDos = *punteroNumero; //le asigno el valor
+    printf("b- %d\n",numeroDos);
+    //*punteroNumero=&numero;
+    //*punteroNumero=55;
+    printf("%d\n",numero);
+    printf("%d\n",numeroDos);
+    printf("%d\n",&numero);
+    printf("%d\n",punteroNumero);
+    printf("%d\n",&punteroNumero);
+    printf("%p\n",&numero);
+    printf("%p\n",punteroNumero); //posicion de memoria de la variable
+    printf("%p\n",&punteroNumero); //posicion de memoria del puntero
+    printf("%d",*punteroNumero); //variable
     return 0;
 }
